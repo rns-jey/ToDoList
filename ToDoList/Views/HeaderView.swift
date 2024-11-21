0612507ddd8450e4831c42b82344cb0716dc923c
JM Arenas <rns.jey@gmail.com>
2024-11-21 18:21:23 +0000

    Update header component

--- ToDoList/Views/HeaderView.swift
+++ ToDoList/Views/HeaderView.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct HeaderView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            ZStack {
+                RoundedRectangle(cornerRadius: 0)
+                    .foregroundColor(Color.pink)
+                    .rotationEffect(Angle(degrees: 15))
+   
+                VStack {
+                    Text("To Do List")
+                        .font(.system(size: 50))
+                        .foregroundStyle(Color.white)
+                        .bold()
+                    
+                    Text("Get things through")
+                        .font(.system(size: 30))
+                        .foregroundStyle(Color.white)
+                }
+                .padding(.top, 30)
+            }
+            .frame(width: geometry.size.width * 3, height: 300)
+            .position(x: geometry.size.width / 2, y: 50)
+            
+            Spacer()
+        }
     }
 }
 
--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -11,29 +11,7 @@ struct LoginView: View {
     var body: some View {
         VStack {
             // Header
-            GeometryReader { geometry in
-                ZStack {
-                    RoundedRectangle(cornerRadius: 0)
-                        .foregroundColor(Color.pink)
-                        .rotationEffect(Angle(degrees: 15))
-       
-                    VStack {
-                        Text("To Do List")
-                            .font(.system(size: 50))
-                            .foregroundStyle(Color.white)
-                            .bold()
-                        
-                        Text("Get things through")
-                            .font(.system(size: 30))
-                            .foregroundStyle(Color.white)
-                    }
-                    .padding(.top, 30)
-                }
-                .frame(width: geometry.size.width * 3, height: 300)
-                .position(x: geometry.size.width / 2, y: 50)
-                
-                Spacer()
-            }
+            HeaderView()
             // Login Form
             
             // Register button
