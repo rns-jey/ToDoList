f5df43e96777d71ba56bf6b39d8a9d5d3688fc04
JM Arenas <rns.jey@gmail.com>
2024-11-21 18:02:30 +0000

    Build final header design

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -15,13 +15,25 @@ struct LoginView: View {
                 ZStack {
                     RoundedRectangle(cornerRadius: 0)
                         .foregroundColor(Color.pink)
+                        .rotationEffect(Angle(degrees: 15))
+       
                     VStack {
+                        Text("To Do List")
+                            .font(.system(size: 50))
+                            .foregroundStyle(Color.white)
+                            .bold()
                         
+                        Text("Get things through")
+                            .font(.system(size: 30))
+                            .foregroundStyle(Color.white)
                     }
+                    .padding(.top, 30)
                 }
-                .frame(width: geometry.size.width * 3)
-            }.frame(height: 300)
-            
+                .frame(width: geometry.size.width * 3, height: 300)
+                .position(x: geometry.size.width / 2, y: 50)
+                
+                Spacer()
+            }
             // Login Form
             
             // Register button
