7bf3bb8aaa697e25acb0124513419731fcc1058f
JM Arenas <rns.jey@gmail.com>
2024-11-21 18:04:45 +0000

    Build header view component

---
+++ ToDoList/Views/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  ToDoList
+//
+//  Created by JM Arenas on 21/11/2024.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
