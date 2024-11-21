8b03cd0c711c22da131a1e1b41f2f6ace4e83dcd
JM Arenas <rns.jey@gmail.com>
2024-11-21 17:06:58 +0000

    Build header block

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -2,14 +2,30 @@
 //  LoginView.swift
 //  ToDoList
 //
-//  Created by JM Arenas on 19/11/2024.
+//  Created by Magnus Sanera on 19/11/2024.
 //
 
 import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // Header
+            GeometryReader { geometry in
+                ZStack {
+                    RoundedRectangle(cornerRadius: 0)
+                        .foregroundColor(Color.pink)
+                    VStack {
+                        
+                    }
+                }
+                .frame(width: geometry.size.width * 3)
+            }.frame(height: 300)
+            
+            // Login Form
+            
+            // Register button
+        }
     }
 }
 
