4fe319f04b19ad1348ca28c496ebbbb17d9be158
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2024-12-05 18:23:52 +0000

    Add main view screen and functionality

--- ToDoList/ViewModels/MainViewModel.swift
+++ ToDoList/ViewModels/MainViewModel.swift
@@ -5,4 +5,22 @@
 //  Created by JM Arenas on 19/11/2024.
 //
 
+import FirebaseAuth
 import Foundation
+
+class MainViewModel: ObservableObject {
+    @Published var currentUserId: String = ""
+    private var handler: AuthStateDidChangeListenerHandle?
+    
+    init() {
+        self.handler = Auth.auth().addStateDidChangeListener { [weak self] _, user in
+            DispatchQueue.main.async {
+                self?.currentUserId = user?.uid ?? ""
+            }
+        }
+    }
+    
+    public var isSignedIn: Bool {
+        return Auth.auth().currentUser != nil
+    }
+}
