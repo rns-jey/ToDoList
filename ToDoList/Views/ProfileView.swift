8ebe6b785ccc9ffeb47926bd719798c10fc6d4c6
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2024-12-08 17:57:36 +0000

    Add the initial button for create function

--- ToDoList/Views/ProfileView.swift
+++ ToDoList/Views/ProfileView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text("This is your profile")
+        NavigationView {
+            VStack {
+                
+            }
+            .navigationTitle("Profile")
+        }
     }
 }
 
