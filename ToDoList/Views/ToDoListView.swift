0e088c9d5ec9bf75bc8c28500efc099e2876a6c2
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2024-12-11 17:22:09 +0000

    Pass user Id

--- ToDoList/Views/ToDoListView.swift
+++ ToDoList/Views/ToDoListView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct ToDoListView: View {
+    @StateObject var viewModel = ToDoListViewModel()
+    
+    private let userId: String
+    
+    init(userId: String) {
+        self.userId = userId
+    }
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -26,5 +34,5 @@ struct ToDoListView: View {
 }
 
 #Preview {
-    ToDoListView()
+    ToDoListView(userId: "")
 }
