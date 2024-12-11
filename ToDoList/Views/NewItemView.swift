624643561201a9d8d5cd348d88b6d2c8b9db8522
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2024-12-11 17:42:45 +0000

    Build initial new item view

--- ToDoList/Views/NewItemView.swift
+++ ToDoList/Views/NewItemView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct NewItemView: View {
+    @StateObject var viewModel = NewItemViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("New item")
+                .font(.system(size: 32))
+                .bold()
+            
+            Form {
+                // Title
+                TextField("Title", text: $viewModel.title)
+                
+                // Due date
+                DatePicker("Due Date", selection: $viewModel.dueDate)
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                
+                // Button
+                TLButton(title: "Save", bgcolor: .blue) {
+                    viewModel.save()
+                }
+                .padding()
+                
+                
+            }
+        }
     }
 }
 
